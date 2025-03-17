9c5d780d4855725cf078ccfda3330c7c564ff07b
huxiao <21794468@qq.com>
2025-03-17 11:54:28 +0800

    3

---
+++ shell/install.sh
@@ -0,0 +1,160 @@
+# Git用户配置信息
+GIT_USER_NAME="小HUI灰"
+GIT_USER_EMAIL="21794468@qq.com"
+DIFY_URL="https://ghfast.top/https://github.com/xiyanluo/dify.git"
+# Pyenv环境信息
+PYENV_URL="https://ghfast.top/https://github.com/pyenv/pyenv.git"
+# Python环境信息
+PYTHON_VERSION="3.12.6"
+PYTHON_ARCHIVE="Python-$PYTHON_VERSION.tar.xz"
+PYTHON_URL="http://wp.dayousoft.com/py/Python-3.12.6.tar.xz"
+# node环境信息
+NODE_URL="https://rpm.nodesource.com/setup_20.x"
+
+install_git(){
+  echo ">>>>>>>>> 1:正在配置Git环境 <<<<<<<<<<<"
+  if ! git --version &> /dev/null; then
+      echo "- Installing git..."
+      # 使用yum安装git
+      sudo yum install -y git
+      if [ $? -ne 0 ]; then
+          echo "- Failed to install git."
+          exit 1
+      fi
+      printSuccess "- [Git]安装成功."
+  fi
+
+  # 输出git版本
+  git_version=$(git --version)
+  echo "- Git version: $git_version"
+
+  # 配置git用户信息
+  git config --global user.name "$GIT_USER_NAME"
+  git config --global user.email "$GIT_USER_EMAIL"
+  echo "- Git global user name and email configured to $GIT_USER_NAME and $GIT_USER_EMAIL."
+
+  # 克隆指定的仓库
+  echo "- 正在拉取[Dify]: $DIFY_URL"
+  git clone $DIFY_URL
+  if [ $? -eq 0 ]; then
+      printSuccess "- [Dify]拉取成功"
+  else
+      printFail "- [Dify]拉取失败"
+#      exit 1
+  fi
+}
+
+install_pyenv(){
+  echo ">>>>>>>>> 2:正在配置Pyenv环境 <<<<<<<<<<<"
+  if [ ! -d "$HOME/.pyenv" ]; then
+      echo "- Installing pyenv..."
+      git clone $PYENV_URL ~/.pyenv
+      if [ $? -eq 0 ]; then
+          printSuccess "- [Pyenv]拉取成功"
+          cd ~/.pyenv && src/configure && make -C src
+          # Add pyenv to bashrc
+          echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+          echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+          echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+          # Source the bashrc file
+          source ~/.bashrc
+          pyenv --version
+      else
+          printFail "- [Pyenv]拉取失败"
+          exit 1
+      fi
+  else
+      echo "- [Pyenv] 检测出已存在"
+      pyenv --version
+  fi
+}
+
+install_Python(){
+  echo ">>>>>>>>> 3:正在配置Python环境 <<<<<<<<<<<"
+  wget $PYTHON_URL
+  # Create cache directory and move the downloaded file
+  mkdir -p "$HOME/.pyenv/cache"
+  mv $PYTHON_ARCHIVE "$HOME/.pyenv/cache/"
+  # Install Python using pyenv
+  pyenv install $PYTHON_VERSION
+  pyenv global $PYTHON_VERSION
+  # Check python version
+  python3 --version
+  # Install development tools and libraries
+  sudo yum install -y openssl-devel zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel wget curl llvm ncurses-devel xz xz-devel libffi-devel gcc gcc-c++ python3-devel
+  sudo yum groupinstall -y "Development Tools"
+  # Upgrade pip and install necessary Python packages
+  python3 -m pip install --upgrade pip setuptools wheel
+  python3 -m pip install grpcio==1.60.0 frozendict kaleido unstructured
+}
+
+install_Poetry(){
+  echo ">>>>>>>>> 3:正在配置Poetry环境 <<<<<<<<<<<"
+  # 检查 poetry 是否已安装
+  if ! command -v poetry &> /dev/null
+  then
+      echo "[poetry]现在将通过 pip 安装..."
+      pip install poetry
+  else
+      echo "[poetry]已安装"
+  fi
+  poetry --version
+}
+
+install_Node(){
+  echo ">>>>>>>>> 4:正在配置Node环境 <<<<<<<<<<<"
+  if command -v node > /dev/null 2>&1 && command -v npm > /dev/null 2>&1; then
+      echo "- Node和npm已经安装。"
+      echo "- Node版本: $(node -v)"
+      echo "- NPM版本: $(npm -v)"
+  else
+      echo "Node未安装，现在开始安装..."
+      # 导入NodeSource Node.js 20.x repo
+      curl -fsSL $NODE_URL | sudo bash -
+      # 安装Node.js
+      if sudo dnf install -y nodejs; then
+          printSuccess "- [Node]安装成功."
+          echo "Node版本: $(node -v)"
+          echo "NPM版本: $(npm -v)"
+      else
+          printFail "Node安装失败。"
+          exit 1
+      fi
+  fi
+}
+getStatus(){
+  echo "pyenv版本: $(pyenv --version)"
+  echo "python3版本: $(python3 --version)"
+  echo "poetry版本: $(poetry --version)"
+  echo "Node版本: $(node -v)"
+  echo "NPM版本: $(npm -v)"
+}
+printSuccess() {
+    echo -e "\033[0;32m$1\033[0m"
+}
+printFail() {
+    echo -e "\033[31m$1\033[0m"
+}
+restart(){
+  install_git
+  install_pyenv
+  install_Python
+  install_Poetry
+  install_Node
+}
+case "$1" in
+  "status") #查看状态
+    getStatus
+    ;;
+  "python")
+    install_Python
+    ;;
+  "pyenv")
+    install_pyenv
+    install_Poetry
+    ;;
+  *)
+    restart
+    ;;
+esac
+exit 0
---
+++ shell/run.sh
@@ -0,0 +1,221 @@
+#das!/bin/bash
+flask_name=flask
+flask_log=flask_log.out
+flask_processes=$(ps aux | grep flask | grep -v grep)
+celery_log=celery_log.out
+celery_processes=$(ps aux | grep celery | grep -v grep)
+# 定义变量
+SOURCE_URL="https://pypi.tuna.tsinghua.edu.cn/simple/"
+SOURCE_NAME="mirrors"
+usage(){
+    echo "Usage: sh 执行脚本.sh [start|stop|restart|status]"
+    exit 1
+}
+#-----------[flask] API服务---------------
+is_exist_flask(){
+  # 通过awk获取每个flask进程的PID
+  if [ -z "$flask_processes" ]; then
+   echo "[${flask_name}]没有找到进程。"
+   return 1
+  else
+   # 输出找到的flask进程
+   echo "[${flask_name}]找到以下进程："
+   echo "$flask_processes"
+   # 通过awk获取每个flask进程的PID
+   pids=$(echo "$flask_processes" | awk '{print $2}')
+   return 0
+  fi
+}
+stop_flask(){
+  is_exist_flask
+  if [ $? -eq "0" ]; then
+    for pid in $pids; do
+      kill -9 $pid
+      if [ $? -eq 0 ]; then
+          echo "[${flask_name}]进程 $pid 已被成功杀掉。"
+      else
+          echo "[${flask_name}]无法杀掉进程 $pid。"
+      fi
+    done
+  fi
+}
+
+start_flask(){
+  cd api
+  nohup poetry run $flask_name run --host 0.0.0.0 --port 5001 --debug > $flask_log 2>&1 &
+  echo ">>> 启动 $flask_name 成功 PID=$! <<<"
+	tail -fn 200 $flask_log
+}
+
+log_flask(){
+  cd api
+  tail -fn 200 $flask_log
+}
+restart_flask(){
+  stop_flask
+  start_flask
+}
+#-------------[celery] Worker服务----------
+is_exist_celery(){
+  # 通过awk获取每个celery进程的PID
+  if [ -z "$celery_processes" ]; then
+   echo "[celery]没有找到进程。"
+   return 1
+  else
+   # 输出找到的celery进程
+   echo "[celery]找到以下进程："
+   echo "$celery_processes"
+   # 通过awk获取每个celery进程的PID
+   cids=$(echo "$celery_processes" | awk '{print $2}')
+   return 0
+  fi
+}
+
+stop_celery(){
+  is_exist_celery
+  if [ $? -eq "0" ]; then
+    for pid in $cids; do
+      kill -9 $pid
+      if [ $? -eq 0 ]; then
+          echo "[celery]进程 $pid 已被成功杀掉。"
+      else
+          echo "[celery]无法杀掉进程 $pid。"
+      fi
+    done
+  fi
+}
+
+start_celery(){
+  cd api
+  nohup poetry run celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail,ops_trace --loglevel INFO > $celery_log 2>&1 & disown
+  echo ">>> 启动 celery 成功 PID=$! <<<"
+  sleep 2; echo "$celery_processes"
+}
+
+log_celery(){
+  cd api
+  tail -fn 200 $celery_log
+}
+
+restart_celery(){
+  stop_celery
+  start_celery
+}
+
+updateGit(){
+  git reset --hard
+  git pull
+}
+updateLib(){
+  # 进入 API 目录
+  cd api
+  poetry lock
+  # 设置 Python 版本为 3.12
+  poetry env use 3.12
+  # 检查是否已经添加了指定的镜像源
+  if ! poetry config repositories.$SOURCE_NAME | grep -q "$SOURCE_URL"; then
+      echo "Adding new source: $SOURCE_NAME..."
+      poetry source add --priority primary $SOURCE_NAME $SOURCE_URL
+      poetry lock --no-update
+  else
+      echo "Source $SOURCE_NAME already exists."
+  fi
+  # 更新依赖锁文件，但不更新依赖本身
+  # 安装依赖
+  poetry install
+
+# cd api && poetry env use 3.12 && poetry install
+}
+installWeb(){
+  cd web
+#  npm cache clean --force
+#  npm config set registry https://registry.npmmirror.com
+#  npm install
+#  npm run build
+  pnpm cache clean
+  pnpm config set registry https://registry.npmmirror.com
+  pnpm install
+  NODE_OPTIONS="--max-old-space-size=4096" pnpm run build
+}
+
+startDocker(){
+  cd docker
+  docker-compose -f docker-compose.middleware.yaml up -d
+}
+
+updateDb(){
+  cd api && poetry run flask db upgrade
+}
+updateVdb(){
+  cd api && poetry run flask vdb-migrate
+}
+
+getStatus(){
+  echo "----------正在查询[Flask]进程-----------"
+  if [ -n "$flask_processes" ]; then
+    echo "找到以下进程:"
+    echo "$flask_processes"
+  else
+    echo -e "\033[31mNot found\033[0m"
+  fi
+  echo "----------正在查询[Celery]进程-----------"
+  if [ -n "$celery_processes" ]; then
+    echo "找到以下进程:"
+    echo "$celery_processes"
+  else
+    echo -e "\033[31mNot found\033[0m"
+  fi
+  echo "----------正在查询Docker服务-----------"
+  echo "找到以下Docker服务:"
+  cd docker && docker-compose ps
+}
+doInstall(){
+  startDocker
+  updateLib
+  updateDb
+  updateVdb
+  installWeb
+}
+
+case "$1" in
+  "stop")
+    stop_flask
+    ;;
+  "log")
+    log_flask
+    ;;
+  "stop2")
+    stop_celery
+    ;;
+  "log2") #打印celery worker日志
+    log_celery
+    ;;
+  "worker") #启动worker
+    restart_celery
+    ;;
+  "git") #git pull
+    updateGit
+    ;;
+  "lib") #更新py依赖包
+    updateLib
+    ;;
+  "web") #编译web包
+    installWeb
+    ;;
+  "status") #查看状态
+    getStatus
+    ;;
+  "docker") #启动docker服务
+    startDocker
+    ;;
+   "db") #pgsql数据库迁移
+    updateDb
+    ;;
+   "vdb") #其它向量数据库更新
+    updateVdb
+    ;;
+  *)
+    restart_flask
+    ;;
+esac
+exit 0
